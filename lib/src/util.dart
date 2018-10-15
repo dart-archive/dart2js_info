053f2e5fa74e8b1f16fff13f8e9daf3f37f84560
Kevin Moore <kevmoo@google.com>
2018-10-15 11:55:16 -0700

    Use a relative path when generating unique IDs for elements
    This does not affect the canonical URI for a library, but it does
    ensure the IDs for libraries and their members are more stable when
    using build systems that rely on temporary directories, like pkg:build
    
    This accomplishes most of the desired behavior from
    https://github.com/dart-lang/sdk/commit/7fe8659613bfbf360d57d12327b7521cd869987a#diff-49250e8c7ce346ad217448560050bf76
    
    which was reverted in
    https://github.com/dart-lang/sdk/commit/2e1c17e5c15766baa8771ad4f29ef188fec52699#diff-49250e8c7ce346ad217448560050bf76

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -52,7 +52,7 @@ Graph<Info> graphFromInfo(AllInfo info) {
 
 /// Provide a unique long name associated with [info].
 // TODO(sigmund): guarantee that the name is actually unique.
-String longName(Info info, {bool useLibraryUri: false}) {
+String longName(Info info, {bool useLibraryUri: false, bool forId: false}) {
   var infoPath = [];
   while (info != null) {
     infoPath.add(info);
@@ -66,7 +66,22 @@ String longName(Info info, {bool useLibraryUri: false}) {
     // assert(!first || segment is LibraryInfo);
     // (today might not be true for for closure classes).
     if (segment is LibraryInfo) {
-      sb.write(useLibraryUri ? segment.uri : segment.name);
+      if (useLibraryUri && forId && segment.uri.isScheme('file')) {
+        var currentBase = Uri.base.toString();
+        var segmentString = segment.uri.toString();
+
+        // If longName is being called to calculate an element ID (forId = true)
+        // then use a relative path for the longName calculation
+        // This allows a more stable ID for cases when files are generated into
+        // per-build temp directories – like with pkg: build
+        if (segmentString.startsWith(currentBase)) {
+          segmentString = segmentString.substring(currentBase.length);
+        }
+
+        sb.write(segmentString);
+      } else {
+        sb.write(useLibraryUri ? segment.uri : segment.name);
+      }
       sb.write('::');
     } else {
       first = false;
